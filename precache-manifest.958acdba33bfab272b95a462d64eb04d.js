aba1aa6ab15b3daeb1041ea7afd6f68b487f36ce
Ott Martens <ott.martens@pipedrive.com>
2020-01-06 01:50:51 +0200

    Updates

---
+++ precache-manifest.958acdba33bfab272b95a462d64eb04d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b80148e9e9fe29d2b26bd022435f67c1",
+    "url": "/file-allocation/index.html"
+  },
+  {
+    "revision": "34dff0dadfc769c0aee0",
+    "url": "/file-allocation/static/css/main.c1967e9b.chunk.css"
+  },
+  {
+    "revision": "0f414ae8109f2e909107",
+    "url": "/file-allocation/static/js/2.f3c60bae.chunk.js"
+  },
+  {
+    "revision": "34dff0dadfc769c0aee0",
+    "url": "/file-allocation/static/js/main.8390cef0.chunk.js"
+  },
+  {
+    "revision": "755ff40d6026e947319b",
+    "url": "/file-allocation/static/js/runtime-main.9d3ccc7e.js"
+  }
+]);
